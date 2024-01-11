d7133e9648996a10af7b5297874e9dbd4dc71c8c
triceratopss <toshiki.12323@gmail.com>
2024-01-11 23:59:22 +0900

    fix: logger middleware

--- middleware/logger.go
+++ middleware/logger.go
@@ -2,22 +2,15 @@ package middleware
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"log/slog"
+	"net"
 	"os"
 	"time"
 
 	"github.com/labstack/echo/v4"
 	echomiddleware "github.com/labstack/echo/v4/middleware"
 )
-
-type AccessLog struct {
-	Severity    string      `json:"severity"`
-	Time        time.Time   `json:"time"`
-	Trace       string      `json:"logging.googleapis.com/trace"`
-	HTTPRequest HTTPRequest `json:"httpRequest"`
-}
 
 type HTTPRequest struct {
 	RequestMethod string   `json:"requestMethod"`
@@ -38,6 +31,16 @@ type Duration struct {
 	Seconds int64 `json:"seconds"`
 }
 
+func MakeDuration(d time.Duration) Duration {
+	nanos := d.Nanoseconds()
+	secs := nanos / 1e9
+	nanos -= secs * 1e9
+	return Duration{
+		Nanos:   int32(nanos),
+		Seconds: secs,
+	}
+}
+
 func Logger() echo.MiddlewareFunc {
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
 		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
@@ -49,72 +52,78 @@ func Logger() echo.MiddlewareFunc {
 		},
 	}))
 	return echomiddleware.RequestLoggerWithConfig(echomiddleware.RequestLoggerConfig{
-		LogMethod:       true,
-		LogURI:          true,
-		LogStatus:       true,
-		LogResponseSize: true,
-		LogUserAgent:    true,
-		LogRemoteIP:     true,
-		LogReferer:      true,
-		LogLatency:      true,
-		LogProtocol:     true,
-		LogError:        true,
-		HandleError:     true,
+		LogMethod:        true,
+		LogURI:           true,
+		LogContentLength: true,
+		LogStatus:        true,
+		LogResponseSize:  true,
+		LogUserAgent:     true,
+		LogRemoteIP:      true,
+		LogReferer:       true,
+		LogLatency:       true,
+		LogProtocol:      true,
+		LogError:         true,
+		HandleError:      true,
 		LogValuesFunc: func(c echo.Context, v echomiddleware.RequestLoggerValues) error {
 			if v.Error == nil {
-				// logger.LogAttrs(context.Background(), slog.LevelInfo, "request",
-				// 	slog.Group("httpRequest",
-				// 		slog.String("requestMethod", v.Method),
-				// 		slog.String("requestUrl", v.URI),
-				// 		slog.Int("status", v.Status),
-				// 		slog.Int64("responseSize", v.ResponseSize),
-				// 		slog.String("userAgent", v.UserAgent),
-				// 		slog.String("remoteIp", v.RemoteIP),
-				// 		slog.String("referer", v.Referer),
-				// 		slog.Float64("latency", v.Latency.Seconds()),
-				// 	),
-				// )
 				httpRequest := HTTPRequest{
 					RequestMethod: v.Method,
 					RequestURL:    v.URI,
 					RequestSize:   v.ContentLength,
 					Status:        v.Status,
-					ResponseSize:  fmt.Sprint(v.ResponseSize),
+					ResponseSize:  fmt.Sprintf("%d", v.ResponseSize),
 					UserAgent:     v.UserAgent,
 					RemoteIP:      v.RemoteIP,
-					ServerIP:      v.RemoteIP,
+					ServerIP:      getServerIP(),
 					Referer:       v.Referer,
-					Latency:       Duration{},
+					Latency:       MakeDuration(v.Latency),
 					Protocol:      v.Protocol,
 				}
 				logger.LogAttrs(context.Background(), slog.LevelInfo, "request",
 					slog.Any("httpRequest", httpRequest),
 				)
-				AccessLog := AccessLog{
-					Severity:    "INFO",
-					Time:        time.Now(),
-					Trace:       "",
-					HTTPRequest: httpRequest,
-				}
-				log, _ := json.Marshal(AccessLog)
-				fmt.Println(string(log))
 			} else {
+				httpRequest := HTTPRequest{
+					RequestMethod: v.Method,
+					RequestURL:    v.URI,
+					RequestSize:   v.ContentLength,
+					Status:        v.Status,
+					ResponseSize:  fmt.Sprintf("%d", v.ResponseSize),
+					UserAgent:     v.UserAgent,
+					RemoteIP:      v.RemoteIP,
+					ServerIP:      getServerIP(),
+					Referer:       v.Referer,
+					Latency:       MakeDuration(v.Latency),
+					Protocol:      v.Protocol,
+				}
 				logger.LogAttrs(context.Background(), slog.LevelError, "request error",
-					slog.Group("httpRequest",
-						slog.String("requestMethod", v.Method),
-						slog.String("requestUrl", v.URI),
-						slog.Int("status", v.Status),
-						slog.Int64("responseSize", v.ResponseSize),
-						slog.String("userAgent", v.UserAgent),
-						slog.String("remoteIp", v.RemoteIP),
-						slog.String("referer", v.Referer),
-						slog.Float64("latency", v.Latency.Seconds()),
-						slog.String("err", v.Error.Error()),
-					),
+					slog.Any("httpRequest", httpRequest),
+					slog.Any("err", v.Error.Error()),
 				)
 			}
 
 			return nil
 		},
 	})
+}
+
+func getServerIP() string {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return ""
+	}
+	for _, i := range ifaces {
+		addrs, err := i.Addrs()
+		if err != nil {
+			return ""
+		}
+		for _, addr := range addrs {
+			if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+				if ipnet.IP.To4() != nil {
+					return ipnet.IP.String()
+				}
+			}
+		}
+	}
+	return ""
 }
