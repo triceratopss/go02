6dcc1ed86ecbbda79eeb2fbbba08992c75086091
triceratopss <toshiki.12323@gmail.com>
2024-01-11 22:05:25 +0900

    test

--- middleware/logger.go
+++ middleware/logger.go
@@ -2,12 +2,41 @@ package middleware
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"log/slog"
 	"os"
+	"time"
 
 	"github.com/labstack/echo/v4"
 	echomiddleware "github.com/labstack/echo/v4/middleware"
 )
+
+type AccessLog struct {
+	Severity    string      `json:"severity"`
+	Time        time.Time   `json:"time"`
+	Trace       string      `json:"logging.googleapis.com/trace"`
+	HTTPRequest HTTPRequest `json:"httpRequest"`
+}
+
+type HTTPRequest struct {
+	RequestMethod string   `json:"requestMethod"`
+	RequestURL    string   `json:"requestUrl"`
+	RequestSize   string   `json:"requestSize,omitempty"`
+	Status        int      `json:"status"`
+	ResponseSize  string   `json:"responseSize,omitempty"`
+	UserAgent     string   `json:"userAgent,omitempty"`
+	RemoteIP      string   `json:"remoteIp,omitempty"`
+	ServerIP      string   `json:"serverIp,omitempty"`
+	Referer       string   `json:"referer,omitempty"`
+	Latency       Duration `json:"latency"`
+	Protocol      string   `json:"protocol"`
+}
+
+type Duration struct {
+	Nanos   int32 `json:"nanos"`
+	Seconds int64 `json:"seconds"`
+}
 
 func Logger() echo.MiddlewareFunc {
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
@@ -30,6 +59,7 @@ func Logger() echo.MiddlewareFunc {
 		LogRemoteIP:     true,
 		LogReferer:      true,
 		LogLatency:      true,
+		LogProtocol:     true,
 		LogError:        true,
 		HandleError:     true,
 		LogValuesFunc: func(c echo.Context, v echomiddleware.RequestLoggerValues) error {
@@ -46,6 +76,26 @@ func Logger() echo.MiddlewareFunc {
 						slog.Float64("latency", v.Latency.Seconds()),
 					),
 				)
+				AccessLog := AccessLog{
+					Severity: "INFO",
+					Time:     time.Now(),
+					Trace:    "",
+					HTTPRequest: HTTPRequest{
+						RequestMethod: v.Method,
+						RequestURL:    v.URI,
+						RequestSize:   v.ContentLength,
+						Status:        v.Status,
+						ResponseSize:  fmt.Sprint(v.ResponseSize),
+						UserAgent:     v.UserAgent,
+						RemoteIP:      v.RemoteIP,
+						ServerIP:      v.RemoteIP,
+						Referer:       v.Referer,
+						Latency:       Duration{},
+						Protocol:      v.Protocol,
+					},
+				}
+				log, _ := json.Marshal(AccessLog)
+				fmt.Println(string(log))
 			} else {
 				logger.LogAttrs(context.Background(), slog.LevelError, "request error",
 					slog.Group("httpRequest",
