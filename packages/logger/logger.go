cb2b0a3328246da2b9c2919d07ea2b78223b4b71
triceratopss <96670142+triceratopss@users.noreply.github.com>
2024-06-30 11:18:02 +0900

    fix logger package

--- packages/logger/logger.go
+++ packages/logger/logger.go
@@ -1,8 +1,12 @@
 package logger
 
 import (
+	"context"
+	"fmt"
 	"log/slog"
 	"os"
+	"runtime"
+	"time"
 )
 
 func Init() {
@@ -26,8 +30,42 @@ func replaceAttr(groups []string, a slog.Attr) slog.Attr {
 	case slog.MessageKey:
 		return slog.String("message", a.Value.String())
 	case slog.SourceKey:
-		return slog.String("sourceLocation", a.Value.String())
+		return slog.Attr{
+			Key:   "logging.googleapis.com/sourceLocation",
+			Value: a.Value,
+		}
 	}
 
 	return a
+}
+
+func log(ctx context.Context, level slog.Level, msg string, args ...any) {
+	logger := slog.Default()
+	if !logger.Enabled(ctx, level) {
+		return
+	}
+
+	var pcs [1]uintptr
+	runtime.Callers(3, pcs[:])
+
+	r := slog.NewRecord(time.Now(), level, msg, pcs[0])
+	r.Add(args...)
+
+	_ = logger.Handler().Handle(ctx, r)
+}
+
+func Info(msg string, args ...any) {
+	log(context.Background(), slog.LevelInfo, msg, args...)
+}
+
+func Infof(format string, args ...any) {
+	log(context.Background(), slog.LevelInfo, fmt.Sprintf(format, args...))
+}
+
+func Error(msg string, args ...interface{}) {
+	log(context.Background(), slog.LevelError, msg, args...)
+}
+
+func Errorf(err error, format string, args ...interface{}) {
+	log(context.Background(), slog.LevelError, fmt.Sprintf(format, args...))
 }
