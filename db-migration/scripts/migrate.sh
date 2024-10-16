b74818b286a99fefb153d0fe221db7abae5b9ab5
triceratopss <96670142+triceratopss@users.noreply.github.com>
2024-10-16 17:25:03 +0900

    fix migration

--- db-migration/scripts/migrate.sh
+++ db-migration/scripts/migrate.sh
@@ -6,7 +6,7 @@ apk --update --no-cache add curl
 curl -o cloud-sql-proxy https://storage.googleapis.com/cloud-sql-connectors/cloud-sql-proxy/v2.1.2/cloud-sql-proxy.linux.amd64
 chmod +x cloud-sql-proxy
 
-./cloud-sql-proxy --private-ip ${INSTANCE_CONNECTION_NAME} & sleep 2;
+./cloud-sql-proxy ${INSTANCE_CONNECTION_NAME} & sleep 2;
 
 URL="postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?search_path=public&sslmode=disable"
 
