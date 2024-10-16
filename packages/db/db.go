e2f9c431bc681e28b6547569b70a4d6ba645f91e
triceratopss <96670142+triceratopss@users.noreply.github.com>
2024-10-16 18:39:55 +0900

    fix db conn

--- packages/db/db.go
+++ packages/db/db.go
@@ -5,12 +5,10 @@ import (
 	"database/sql"
 	"fmt"
 	"go02/packages/config"
-	"net/url"
 	"time"
 
 	"github.com/uptrace/bun"
 	"github.com/uptrace/bun/dialect/pgdialect"
-	"github.com/uptrace/bun/driver/pgdriver"
 	"github.com/uptrace/bun/extra/bundebug"
 
 	_ "github.com/lib/pq"
@@ -18,17 +16,18 @@ import (
 
 func OpenDB() (*bun.DB, error) {
 
-	encodedDBUser := url.QueryEscape(config.Config.DBUser)
-	encodedDBPassword := url.QueryEscape(config.Config.DBPassword)
-	encodedDBName := url.QueryEscape(config.Config.DBName)
-	dsn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable",
-		encodedDBUser,
-		encodedDBPassword,
+	dsn := fmt.Sprintf("user=%s password=%s database=%s host=%s port=%s sslmode=disable",
+		config.Config.DBUser,
+		config.Config.DBPassword,
+		config.Config.DBName,
 		config.Config.DBHost,
 		config.Config.DBPort,
-		encodedDBName,
 	)
-	sqlDB := sql.OpenDB(pgdriver.NewConnector(pgdriver.WithDSN(dsn)))
+
+	sqlDB, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, fmt.Errorf("sql.Open: %w", err)
+	}
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
